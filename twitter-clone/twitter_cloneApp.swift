5533e841003244bdd662555761e427ea05388b9c
torekhanmukhtarov <nahkerot@gmail.com>
2024-03-30 12:05:43 +0500

    Initial Commit

---
+++ twitter-clone/twitter_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitter_cloneApp.swift
+//  twitter-clone
+//
+//  Created by Torekhan Mukhtarov on 30.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct twitter_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
